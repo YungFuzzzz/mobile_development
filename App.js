795d3e7f02f7feee1a6c3e1d4b9d146ccc8e4170
YungFuzzzz <faisalcollinet01@gmail.com>
2025-03-23 15:26:07 +0100

    refractor: Cleaned up code.

--- App.js
+++ App.js
@@ -14,14 +14,12 @@ import ShopScreen from './screens/ShopScreen';
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-// HomeStack: Voor de HomeScreen (zonder producten)
 const HomeStack = () => (
   <Stack.Navigator>
     <Stack.Screen name="Home" component={HomeScreen} />
   </Stack.Navigator>
 );
 
-// ShopStack: Voor de ShopScreen en ProductDetails
 const ShopStack = () => (
   <Stack.Navigator>
     <Stack.Screen name="Shop" component={ShopScreen} />
@@ -29,7 +27,6 @@ const ShopStack = () => (
   </Stack.Navigator>
 );
 
-// Main App Component
 export default function App() {
   const [fontsLoaded] = useFonts({
     MetropolisRegular: require('./assets/fonts/metropolis.regular.otf'),
@@ -60,12 +57,12 @@ export default function App() {
       >
         <Tab.Screen
           name="Home"
-          component={HomeStack} // HomeStack voor de HomeScreen
+          component={HomeStack}
           options={{ headerShown: false }}
         />
         <Tab.Screen
           name="Shop"
-          component={ShopStack} // ShopStack met ShopScreen en ProductDetails
+          component={ShopStack}
           options={{ headerShown: false }}
         />
         <Tab.Screen
